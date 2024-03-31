8123c8b5efd236ea016b6320e9ad43ae7280edfc
Cas van Hees <146176177+CasvHees@users.noreply.github.com>
2024-03-31 18:01:40 +0200

    distribution function to assign velocities to cells
    Based on [Durham2013]

---
+++ util/util.py
@@ -0,0 +1,57 @@
+import numpy as np
+import scipy.interpolate
+import csv
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+rc('text', usetex=True)
+
+def rand_unit_vect_2D():
+    """Generate a unit 2-vector with random direction"""
+    xy = np.random.normal(size=2)
+    mag = sum(i**2 for i in xy) ** 0.5
+    return xy / mag
+
+def swim_speed_dist(num_particles, dist='swim_speed_distribution.csv'):
+    """Produce a random swim speed for each particle based on the swim speed distribution for H. Akashiwo given in [Durham2013]"""
+
+    # import the histogram (contains particle speed dist in um/s)
+    with open(dist, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+        bins = []
+        counts = []
+        for row in reader:
+            bins.append(row[0])
+            counts.append(row[1])
+
+    # generate the PDF
+    cum_counts = np.cumsum(counts)
+    bin_width = 3
+    x = cum_counts * bin_width
+    y = bins
+    pdf = scipy.interpolate.interp1d(x, y)
+    b = np.zeros(num_particles)
+    for i in range(len(b)):
+        u = np.random.uniform(x[0], x[-1])
+        b[i] = pdf(u)  # could convert in to meters if you want
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(b, bins=100, density=True, alpha=1, color='black')
+    plt.xlabel('Swim Speed (um/s)')
+    plt.ylabel('PDF')
+    plt.xlim([0, np.max(b)])
+    plt.title('Swim Speed Distribution for H. Akashiwo')
+    plt.grid(True)
+
+    median_speed = np.median(b)
+    mean_speed = np.mean(b)
+    std_speed = np.std(b)
+    
+    textbox_content = f'Median: {median_speed:.3f}\nMean: {mean_speed:.3f}\nStd: {std_speed:.3f}'
+    plt.text(0.98, 0.95, textbox_content, fontsize=13, horizontalalignment='right', verticalalignment='top', transform=plt.gca().transAxes, bbox=dict(facecolor='white', alpha=1))
+    plt.savefig("PDF_Akashiwo.png",dpi=400)
+    plt.show()
+
+swim_speed_dist(10000)
+
+
